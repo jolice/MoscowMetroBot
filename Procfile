b63c6c4608dc41cb0459d2e2b653229cf0fff977
Vladimir Alexeev <25826296+riguron@users.noreply.github.com>
2019-09-24 17:10:55 +0300

    Heroku procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: sh build.sh
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mvn clean install && mvn clean install spring-boot:repackage -pl Bootstrap
+mvn clean install && mvn clean install spring-boot:repackage -pl Bootstrap && java -jar Bootstrap/target/Bootstrap-1.0.jar
