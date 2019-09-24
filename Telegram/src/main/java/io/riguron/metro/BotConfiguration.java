2fd4cd4ae5df1427b3aa2738c791231b5cf9b1b0
Vladimir Alexeev <25826296+riguron@users.noreply.github.com>
2019-09-24 16:35:44 +0300

    Replaced config fiel with env variables

--- Telegram/src/main/java/io/riguron/metro/BotConfiguration.java
+++ Telegram/src/main/java/io/riguron/metro/BotConfiguration.java
@@ -2,7 +2,6 @@ package io.riguron.metro;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -10,7 +9,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import javax.annotation.PostConstruct;
 
 @Configuration
-@PropertySource("classpath:bot.properties")
 public class BotConfiguration {
 
     static {
